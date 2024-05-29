53b3f397848ebdb5de22d6c9bd03255a766f5b10
beec1000 <beec1000.minecraft@gmail.com>
2024-05-29 12:12:42 +0200

    Add project files.

---
+++ Wpf_Autok/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Wpf_Autok
+{
+    internal class Car
+    {
+        public string BrandName { get; set; }
+        public string Type { get; set; }
+        public int Vintage { get; set; }
+        public string Color { get; set; }
+        public string Source { get; set; }
+
+        public Car(string name, string type, int vintage, string color, string source)
+        {
+            BrandName = name;
+            Type = type;
+            Vintage = vintage;
+            Color = color;
+            Source = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"../../../src/IMAGES/{source}"));
+        }
+
+        public static List<Car> FromFile(string path)
+        {
+            List<Car> cars = new List<Car>();
+
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(';');
+
+                string carName = parts[0];
+                string carType = parts[1];
+                int carVintage = int.Parse(parts[2]);
+                string carColor = parts[3];
+                string carImageS = parts[4];
+
+                Car car = new Car(carName, carType, carVintage, carColor, carImageS);
+                cars.Add(car);
+            }
+
+            return cars;
+        }
+
+        public override string ToString()
+        {
+            return $"{BrandName} {Type} | {Vintage} | {Color}";
+        }
+    }
+}
---
+++ Wpf_Autok/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Autok
+{
+    public partial class MainWindow : Window
+    {
+        private List<Car> cars = new List<Car>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cars = Car.FromFile(@"..\..\..\src\cars.txt");
+            DataContext = this;
+            List.ItemsSource = cars;
+
+        }
+
+        private void AddToFavoriteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = List.SelectedItem;
+
+            if (selected != null)
+            {
+                FavoriteList.Items.Add(selected);
+            }
+        }
+
+        private void DeleteFromFavoriteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = List.SelectedItem;
+            if (selected != null && FavoriteList.Items.Contains(selected))
+            {
+                FavoriteList.Items.Remove(selected);
+            }
+            else
+            {
+                MessageBox.Show("This car is not in Favorites!", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
